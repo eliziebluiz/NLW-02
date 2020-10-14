d1feea0e00b7830183c7fe3a380353f1869bbfff
Elizieb <elizieb.l3@gmail.com>
2020-10-14 02:34:16 -0300

    Primeiro dia de NWL#03

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/pages/landing.css'
+
+import './styles/global.css'
+
+import Routes from './routes'
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
